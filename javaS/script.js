5192be902ce71530fdb7433bf906f9b933a95307
ym7001 <ssym11301007@yahoo.co.jp>
2019-07-19 11:24:29 +0900

    7/19

---
+++ javaS/script.js
@@ -0,0 +1,32 @@
+function menseki(hankei){
+	return "面積は"+(hankei*hankei*3.14)+"cm2です。";
+	}
+	document.write(menseki(5));
+	document.write("<br>");
+	document.write(menseki(7));
+	document.write("<br>");
+	document.write(menseki(10));
+
+	document.write("<br>")
+	document.write("<br>")
+
+
+function goukei(group,otona,kodomo){
+	return group+"グループの合計金額は"+(otona*500+kodomo*200)+"円です。";
+	}
+	 document.write(goukei("A",2,3));
+	 document.write("<br>");
+	 document.write(goukei("B",1,5));
+	 document.write("<br>");
+	 document.write(goukei("C",3,7));	
+
+
+
+
+
+
+
+
+
+
+
